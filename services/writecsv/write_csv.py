291ac227bdb905fa227c3a778e09ed0d2ff4e81f
peilong <peilong@zhuang.com>
2019-06-28 15:18:00 +0800

    update

--- services/writecsv/write_csv.py
+++ services/writecsv/write_csv.py
@@ -2,11 +2,14 @@ import csv
 import datetime
 from conf.conf import CSV_FILE_NAME, CSV_FILE_NAME_NOTIME
 
-def Write_Csv(datas,score_datas):
+def Write_Csv(datas,score_datas,fields):
     file_name = CSV_FILE_NAME + str(datetime.datetime.now().strftime('%Y-%m-%d')) + '.csv'
     csv_file = open(file_name, "a+", encoding="utf-8", newline='')
     writer = csv.writer(csv_file)
-    writer.writerow(['tid_url', 'topicid', 'score', 'replies', 'favorite', 'support', 'historyPV', 'yesterdayPV', 'yesterdayUV', 'dateline'])
+    fields_list = ['tid_url', 'topicid', 'score']
+    for field in fields:
+        fields_list.append(field)
+    writer.writerow(fields_list)
     for data, score_data in zip(datas,score_datas):
         data = list(data)
         score_data = list(score_data)
@@ -14,11 +17,14 @@ def Write_Csv(datas,score_datas):
         score_data.extend(data)
         writer.writerow(score_data)
 
-def Write_Csv_notime(datas,score_datas):
+def Write_Csv_notime(datas,score_datas,fields):
     file_name = CSV_FILE_NAME_NOTIME + str(datetime.datetime.now().strftime('%Y-%m-%d')) + '.csv'
     csv_file = open(file_name, "a+", encoding="utf-8", newline='')
     writer = csv.writer(csv_file)
-    writer.writerow(['tid_url', 'topicid', 'score', 'replies', 'favorite', 'support', 'historyPV', 'yesterdayPV', 'yesterdayUV'])
+    fields_list = ['tid_url', 'topicid', 'score']
+    for field in fields:
+        fields_list.append(field)
+    writer.writerow(fields_list)
     for data, score_data in zip(datas,score_datas):
         data = list(data)
         score_data = list(score_data)
