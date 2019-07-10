b593ef7f0349621f4935e5f893b2bae06541305c
peilong <peilong@zhuang.com>
2019-07-10 15:11:43 +0800

    update

--- services/writecsv/write_csv.py
+++ services/writecsv/write_csv.py
@@ -3,10 +3,23 @@ import datetime
 import time
 
 from services.decorators.decorator import Decorators_time
-from conf.conf import SAVE_CSV_DIR_PATH
+from conf.conf import SAVE_CSV_DIR_PATH, BASIC_PATH
+
+def get_title_dic():
+    with open(BASIC_PATH+"subject.txt", "r", encoding="utf-8") as fp:
+        data_dic = {}
+        num = 0
+        for data in fp.readlines():
+            if num == 0:
+                num = 1
+                continue
+            data_list = data.split(",")
+            data_dic[data_list[0]] = data_list[1].strip("\n")
+    return data_dic
 
 @Decorators_time
 def Write_Csv(datas, score_datas, fields, filename, fields_header):
+    title_dic = get_title_dic()
     file_name = SAVE_CSV_DIR_PATH + '/' + filename + '_' + str(datetime.datetime.now().strftime('%Y-%m-%d')) + '.csv'
     csv_file = open(file_name, "a+", encoding="utf-8", newline='')
     writer = csv.writer(csv_file)
@@ -14,6 +27,7 @@ def Write_Csv(datas, score_datas, fields, filename, fields_header):
     for field in fields_header:
         fields_list.append(field)
     fields_list.append("scroce")
+    fields_list.append("title")
     for field in fields:
         fields_list.append(field)
     writer.writerow(fields_list)
@@ -25,6 +39,7 @@ def Write_Csv(datas, score_datas, fields, filename, fields_header):
         data = list(data)
         score_data = list(score_data)
         score_data[0] = "https://bbs.feng.com/read-htm-tid-" + str(int(float(score_data[0]))) + ".html"
+        score_data.append(title_dic[str(int(float(score_data[0])))])
         if flag is not None:
             timeArray = time.localtime(int(score_data[2]))
             score_data[2] = str(time.strftime("%Y--%m--%d %H:%M:%S", timeArray))
@@ -33,6 +48,7 @@ def Write_Csv(datas, score_datas, fields, filename, fields_header):
 
 @Decorators_time
 def Write_Csv_notime(datas, score_datas, fields, filename, fields_header):
+    title_dic = get_title_dic()
     file_name = SAVE_CSV_DIR_PATH + '/' + filename + '_' + str(datetime.datetime.now().strftime('%Y-%m-%d')) + '.csv'
     csv_file = open(file_name, "a+", encoding="utf-8", newline='')
     writer = csv.writer(csv_file)
@@ -40,6 +56,7 @@ def Write_Csv_notime(datas, score_datas, fields, filename, fields_header):
     for field in fields_header:
         fields_list.append(field)
     fields_list.append("scroce")
+    fields_list.append("title")
     for field in fields:
         fields_list.append(field)
     writer.writerow(fields_list)
@@ -51,6 +68,7 @@ def Write_Csv_notime(datas, score_datas, fields, filename, fields_header):
         data = list(data)
         score_data = list(score_data)
         score_data[0] = "https://bbs.feng.com/read-htm-tid-" + str(int(float(score_data[0]))) + ".html"
+        score_data.append(title_dic[str(int(float(score_data[0])))])
         if flag is not None:
             timeArray = time.localtime(int(score_data[2]))
             score_data[2] = str(time.strftime("%Y--%m--%d %H:%M:%S", timeArray))
